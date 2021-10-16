1abea97c58e033e90cee9859c6d84cdb66d720da
nareshgajul23 <87167811+nareshgajul23@users.noreply.github.com>
2021-10-16 20:28:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');//<p></p>
+    paragraph.classList.add('paragraph-styling');//<p class=""></p>
+    paragraph.innerText = inputField.value;//<p class="">hjhj</p>
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
+
